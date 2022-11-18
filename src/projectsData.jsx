9a190955a7c68fe5c09e1d45e726340d45a03016
msabdalaal <medosayed239@gmail.com>
2022-11-18 06:45:59 +0200

    solving images

--- src/projectsData.jsx
+++ src/projectsData.jsx
@@ -2,7 +2,7 @@ export default [
   {
     id: 0,
     title: "Quizzical",
-    image: "/Work-5.png",
+    image: "/MohamedSayed/Work-5.png",
     description: "Trivia Quizz Game",
     url: "https://msabdalaal.github.io/Quizzical/",
     github: "https://github.com/msabdalaal/Quizzical",
@@ -11,7 +11,7 @@ export default [
   {
     id: 1,
     title: "ToDo",
-    image: "/Work-1.png",
+    image: "/MohamedSayed/Work-1.png",
     description: "Todo List App",
     url: "https://msabdalaal.github.io/todo-app/",
     github: "https://github.com/msabdalaal/todo-app",
@@ -20,7 +20,7 @@ export default [
   {
     id: 2,
     title: "Tenzies Game",
-    image: "/Work-8.png",
+    image: "/MohamedSayed/Work-8.png",
     description: "Tenzies Game With Cool Winning Animation",
     url: "https://msabdalaal.github.io/TenziesGameReact/",
     github: "https://github.com/msabdalaal/TenziesGameReact",
@@ -29,7 +29,7 @@ export default [
   {
     id: 3,
     title: "Rock Paper Scissors",
-    image: "/Work-3.png",
+    image: "/MohamedSayed/Work-3.png",
     description: "Rock Paper Scissors Fun Game",
     url: "https://msabdalaal.github.io/rock-paper-scissors-master/",
     github: "https://github.com/msabdalaal/rock-paper-scissors-master",
@@ -38,7 +38,7 @@ export default [
   {
     id: 4,
     title: "Meme Generator",
-    image: "/Work-7.png",
+    image: "/MohamedSayed/Work-7.png",
     description: "Meme Generator App With Adding Text",
     url: "https://msabdalaal.github.io/MemeGeneratorReact/",
     github: "https://github.com/msabdalaal/MemeGeneratorReact",
@@ -47,7 +47,7 @@ export default [
   {
     id: 5,
     title: "Calculator",
-    image: "/Work-2.png",
+    image: "/MohamedSayed/Work-2.png",
     description: "Multitheme calculator App",
     url: "https://msabdalaal.github.io/calculator-app/",
     github: "https://github.com/msabdalaal/calculator-app",
@@ -56,7 +56,7 @@ export default [
   {
     id: 6,
     title: "Interactive Card",
-    image: "/Work-6.png",
+    image: "/MohamedSayed/Work-6.png",
     description: "Interactive Card Info Form",
     url: "https://msabdalaal.github.io//InteractiveInfoForm/",
     github: "https://github.com/msabdalaal/InteractiveInfoForm",
@@ -65,7 +65,7 @@ export default [
   {
     id: 7,
     title: "Product Page",
-    image: "/Work-4.png",
+    image: "/MohamedSayed/Work-4.png",
     description: "Ecommerce Online Shop Iteractive Page",
     url: "https://msabdalaal.github.io/Ecommerce-Product-Page/",
     github: "https://github.com/msabdalaal/Ecommerce-Product-Page",
