e84ff3237a648d26366049fa86a09d678de816ae
msabdalaal <medosayed239@gmail.com>
2022-11-23 07:56:20 +0200

    add drag nav feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,34 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faIdCard,
-  faArrowUp,
-  faArrowDown,
   faBriefcase,
   faMessage,
+  faUpDownLeftRight,
 } from "@fortawesome/free-solid-svg-icons";
 import logo from "../assets/Nav-Logo-White.png";
 import "../css/NavBar.css";
+import React, { useState } from "react";
 export default function NavBar({ visibleSection }) {
+  let [postion, setPostion] = useState({
+    x: "94%",
+    y: "30%",
+  });
+  function handleDrag(e) {
+    setPostion({
+      x: e.clientX - 30,
+      y: e.clientY - 30,
+    });
+  }
   return (
-    <nav>
+    <nav style={{ left: postion.x, top: postion.y }}>
       {/* <div className="closeNav">
         <FontAwesomeIcon icon={faArrowUp}></FontAwesomeIcon>
         <FontAwesomeIcon icon={faArrowDown} className="hide"></FontAwesomeIcon>
       </div> */}
       <ul>
+        <li onDragEnd={handleDrag} draggable="true" className="drag">
+          <FontAwesomeIcon icon={faUpDownLeftRight}></FontAwesomeIcon>
+        </li>
         <li>
           <a href="#home" className={`${visibleSection == "home" && "active"}`}>
             <img src={logo} alt="Logo" />
